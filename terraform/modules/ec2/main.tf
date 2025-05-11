b68e951e82527132c17475d9ada88083c0e77db2
hafez adel <103792766+hafez599@users.noreply.github.com>
2025-05-11 16:44:31 +0300

    added user data script

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -32,7 +32,20 @@ resource "aws_instance" "jenkins_server" {
   vpc_security_group_ids      = var.vpc_security_group_ids
   associate_public_ip_address = true
 
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel zlib-devel wget make
+              cd /usr/src
+              sudo wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
+              sudo tar xzf Python-3.8.12.tgz
+              cd Python-3.8.12
+              sudo ./configure --enable-optimizations
+              sudo make altinstall
+              EOF
+
   tags = merge(var.tags, {
     Name = "jenkins-server"
   })
 }
+
